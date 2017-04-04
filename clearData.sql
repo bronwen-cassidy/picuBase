edf8c355440edb894b7028c353bf3cfa2a839fe6
bronwen-cassidy <bronwen.cassidy@gmail.com>
2017-04-04 15:17:32 +0100

    The multi widgets is done, didn't end up using an extension, more was going on than i could work out in a short period of time, what is there seems to be acceptable, for now anyway, now time for charting

---
+++ clearData.sql
@@ -0,0 +1,10 @@
+delete from picu_admission_admission_diagnosis;
+delete from picu_admission_positive_cultures;
+delete from picu_admission;
+delete from picu_culture;
+delete from picu_diagnosis;
+delete from picu_diagnosticcode;
+delete from picu_patient;
+delete from picu_picu;
+delete from picu_selectionvalue;
+delete from picu_selectiontype;
